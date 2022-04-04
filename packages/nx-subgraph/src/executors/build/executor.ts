037d95eea58727b2731b4d7fdfe0065605c0c705
Alex Keating <keating.dev@protonmail.com>
2022-04-04 15:27:19 -0400

    Add build executor

--- packages/nx-subgraph/src/executors/build/executor.ts
+++ packages/nx-subgraph/src/executors/build/executor.ts
@@ -1,8 +1,23 @@
-import { DeployExecutorSchema } from './schema';
-import { exec } from 'child_process';
+import { BuildExecutorSchema } from './schema';
+import { exec } from '@actions/exec';
 
-export default async function runExecutor(options: DeployExecutorSchema) {
+// 1. Codegen subgraph for all specified networks
+export default async function runExecutor(options: BuildExecutorSchema) {
   console.log('Executor ran for Deploy', options);
+  const codegenCode = await exec('yarn run codegen');
+  if (codegenCode !== 0) {
+    return {
+      success: false,
+      exitCode: codegenCode,
+    };
+  }
+  const buildCode = await exec('yarn run build');
+  if (buildCode !== 0) {
+    return {
+      success: false,
+      exitCode: buildCode,
+    };
+  }
   return {
     success: true,
   };
