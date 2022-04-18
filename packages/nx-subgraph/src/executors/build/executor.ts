3513e12f13b3edd34cef8e98060601f4c3eaa613
Alex Keating <keating.dev@protonmail.com>
2022-04-18 11:15:20 -0400

    Update build command

--- packages/nx-subgraph/src/executors/build/executor.ts
+++ packages/nx-subgraph/src/executors/build/executor.ts
@@ -1,17 +1,21 @@
 import { BuildExecutorSchema } from './schema';
 import { exec } from '@actions/exec';
+import { ExecutorContext } from '@nrwl/devkit';
 
 // 1. Codegen subgraph for all specified networks
-export default async function runExecutor(options: BuildExecutorSchema) {
+export default async function runExecutor(
+  options: BuildExecutorSchema,
+  context: ExecutorContext
+) {
   console.log('Executor ran for Deploy', options);
-  const codegenCode = await exec('graph codegen');
+  const codegenCode = await exec(`graph codegen ${context.cwd}/subgraph.yaml`);
   if (codegenCode !== 0) {
     return {
       success: false,
       exitCode: codegenCode,
     };
   }
-  const buildCode = await exec('graph build');
+  const buildCode = await exec(`graph build ${context.cwd}/subgraph.yaml`);
   if (buildCode !== 0) {
     return {
       success: false,
