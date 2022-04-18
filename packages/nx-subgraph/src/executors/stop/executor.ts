f28ffef425ceb8aad7b9783f7c165d8ac40b313b
Alex Keating <keating.dev@protonmail.com>
2022-04-18 18:58:01 -0400

    Add stop

---
+++ packages/nx-subgraph/src/executors/stop/executor.ts
@@ -0,0 +1,20 @@
+import { StopExecutorSchema } from './schema';
+import { exec } from '@actions/exec';
+import { ExecutorContext } from '@nrwl/devkit';
+import * as path from 'path';
+
+// 1. Codegen subgraph for all specified networks
+export default async function runExecutor(
+  options: StopExecutorSchema,
+  context: ExecutorContext
+) {
+  const parsedPath = path.parse(__dirname);
+  const composePath = path.join(parsedPath.dir, 'serve', 'docker-compose.yml');
+  const codegenCode = await exec(`docker compose stop -f ${composePath}`);
+  if (codegenCode !== 0) {
+    throw new Error(`Failed to stop subgraph node ${codegenCode}`);
+  }
+  return {
+    success: true,
+  };
+}
