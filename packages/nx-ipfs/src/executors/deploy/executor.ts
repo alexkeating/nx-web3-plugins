7df2a2fa0ace4a8e587630b17ba236d73618c755
Alex Keating <keating.dev@protonmail.com>
2022-10-25 10:10:09 -0400

    Change params

--- packages/nx-ipfs/src/executors/deploy/executor.ts
+++ packages/nx-ipfs/src/executors/deploy/executor.ts
@@ -4,20 +4,24 @@ import { updateDNSLink, getDNSRecord } from './utils/cloudflare';
 
 export default async function runExecutor(options: DeployExecutorSchema) {
   console.log('Executor ran for Build', options);
+  const pinataApiKey = process.env[options.pinataApiKey];
+  const pinataSecretApiKey = process.env[options.pinataSecretApiKey];
   try {
     const hash = await deployPinata({
       verbose: options.verbose,
       removeOld: options.removeOldDeploy,
       pinName: options.pinName,
       buildPath: options.uploadPath,
-      apiKey: options.pinataApiKey,
-      secretApiKey: options.pinataSecretApiKey,
+      apiKey: pinataApiKey,
+      secretApiKey: pinataSecretApiKey,
     });
     console.log(`Frontend deployed to hash ${hash}`);
     if (options.cloudflare) {
       console.log('Switching cloudflare DNS');
+      const cloudflareZoneID = process.env[options.cloudflare.cloudflareZoneID];
+      const cloudflareToken = process.env[options.cloudflare.cloudflareToken];
       const resp = await getDNSRecord({
-        cloudflareZoneID: options.cloudflare.cloudflareZoneID,
+        cloudflareZoneID: cloudflareZoneID,
         recordName: options.cloudflare.recordName,
         recordDomain: options.cloudflare.recordDomain,
       });
@@ -30,8 +34,8 @@ export default async function runExecutor(options: DeployExecutorSchema) {
       for (const record of body.result) {
         await updateDNSLink({
           recordId: record.id,
-          cloudflareZoneID: options.cloudflare.cloudflareZoneID,
-          cloudflareToken: options.cloudflare.cloudflareToken,
+          cloudflareZoneID: cloudflareZoneID,
+          cloudflareToken: cloudflareToken,
           recordName: options.cloudflare.recordName,
           recordDomain: options.cloudflare.recordDomain,
           hash,
